a960d01a13be2e2717523750ee398099b140d171
Bright-ML <285587851@qq.com>
2020-03-20 16:30:13 +0800

    second

--- SmartConfig.ino
+++ SmartConfig.ino
@@ -1,27 +1,54 @@
 #include <ESP8266WiFi.h>
 
-
 void setup()
 {
-    smartConfig ();//执行一键配网
+  Serial.begin(115200);  
+  smartConfig();//这里调用一键配网函数,替换上面的普通配网
 }
-
-void loop() {
-    // put your main code here, to run repeatedly:
-}
-
-
-void smartConfig() //一键配网函数
+void loop()
 {
-  WiFi.mode(WIFI_STA);
-  Serial.println("\r\nWait for Smartconfig");
-  WiFi.beginSmartConfig();//SmartConfig配网开始
-  while (!WiFi.smartConfigDone())
+  // put your main code here, to run repeatedly:
+}
+
+//==========将下列代码添加到需要一键配网项目代码的最后==========
+void smartConfig()
+{
+  WiFi.begin();
+  for (int i = 0; i < 3; i++)
   {
-    delay(500);
-    Serial.println(".");
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      Serial.println("AutoConfig Success");
+      Serial.printf("SSID:%s\r\n", WiFi.SSID().c_str());
+      Serial.printf("PSW:%s\r\n", WiFi.psk().c_str());
+      //WiFi.printDiag(Serial); //打印关键的Wi-Fi诊断信息，信息比较多
+      break;
+    }
+    else
+    {
+      Serial.println("AutoConfig Waiting...");
+      //Serial.println(WiFi.status());
+      delay(1000);
+    }
   }
-  Serial.println("SmartConfig Success");
-  Serial.printf("SSID:%s\r\n",WiFi.SSID().c_str());
-  Serial.printf("PSW:%s\r\n",WiFi.psk().c_str());
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("AutoConfig Faild!" );
+    WiFi.mode(WIFI_STA);
+    Serial.println("\r\nWait for Smartconfig");
+    WiFi.beginSmartConfig();
+    while (1)
+    {
+      Serial.print(".");
+      if (WiFi.smartConfigDone())
+      {
+        Serial.println("SmartConfig Success");
+        Serial.printf("SSID:%s\r\n", WiFi.SSID().c_str());
+        Serial.printf("PSW:%s\r\n", WiFi.psk().c_str());
+        WiFi.setAutoConnect(true);  // 设置自动连接
+        break;
+      }
+      delay(3000);
+    }
+  }
 }
